cbcf455f8bad9a79188e153eb42bacbd1858cbb9
Lauren Johnston <hilotraining@outlook.com>
2023-01-30 18:17:23 -0700

    done header and nav

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app           = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// The in-memory database of tweets. It's a basic object with an array in it.
+// The in-memory database of tweets. It's a basic object with an array in it. Requiring a JSON file automatically parses it and returns the data. The tweets are then written out in data-files
 const db = require("./lib/in-memory-db");
 
 // The `data-helpers` module provides an interface to the database of tweets.
