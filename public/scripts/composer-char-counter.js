9d84b0f9a46e8f10b0ad71108f5460f94684994a
Lauren Johnston <hilotraining@outlook.com>
2023-01-31 10:47:03 -0700

    finished char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,30 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  console.log('ready');
+  console.log(' the DOM is ready to be manipulated with jQuery');
 });
+
+
+// CHARACTER COUNTER: variables
+const myTweet = document.getElementById("tweet-text");
+let wordCount = document.getElementById("counter");
+
+
+//CHARACTER COUNTER: event listener
+myTweet.addEventListener("keyup", function() {
+  const maxChar = 140;
+  const charCount = this.value.length; //counts characters starting at 0.
+  const charactersLeft = maxChar - charCount;
+  wordCount.innerText = charactersLeft;
+
+  if (charactersLeft < 0) {
+    document.getElementById("counter").style.color = "#c94642";
+  } else {
+    document.getElementById("counter").style.color = "#545149";
+  }
+});
+
+// myTweet.addEventListener("keyup", function() {
+//   const characters = this.value.length; //counts characters starting at 0.
+//   const charactersLeft = wordCount.innerText - characters;
+//   wordCount.innerText = charactersLeft;
+// });
