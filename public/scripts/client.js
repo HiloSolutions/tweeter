97717109f37b0d113a6904ab097bbb1c968eb557
Lauren Johnston <hilotraining@outlook.com>
2023-02-01 14:07:06 -0700

    done render function client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,20 +4,55 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
 
-  // // NEW TWEET
-  //takes in tweet object, returns a tweet <article>
+
+
+  // RENDER TWEET: takes an array of tweet objects, appends to #tweets-container
+  const renderTweets = function(arr) {
+    arr.forEach(e => {
+      const $tweet = createTweetElement(e);
+      $('#tweet-container').append($tweet);
+    });
+  };
+
+
+  //NEW TWEET: takes in tweet object, returns a tweet <article>
   const createTweetElement = function(tweet) {
     const $tweet = $(
       `<article class="tweet">
         <header>
-          <img src="${tweetData.user.avatars}">
-          <div>${tweetData.user.name}</div>
-          <div class="user-handle">${tweetData.user.handle}</div>
+          <img src="${tweet.user.avatars}">
+          <div>${tweet.user.name}</div>
+          <div class="user-handle">${tweet.user.handle}</div>
         </header>
-        <p>${tweetData.content.text}</p>
+        <p>${tweet.content.text}</p>
         <footer>
-          <div>${tweetData.created_at}</div>
+          <div>${tweet.created_at}</div>
           <div>
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
@@ -29,30 +64,5 @@ $(document).ready(function() {
     return $tweet;
   };
 
-
-
-  // // RENDER TWEET
-  // takes an array of tweet objects, appends each one to the #tweets-container
-
-
-
-  // // REMOVE BEFORE SUBMISSION
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  };
-
-  const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('.tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
 });
